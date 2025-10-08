53c54c0448f1ef976893225bba8b6714081120d7
Christiaan Huisman <huismanchristiaanwork@gmail.com>
2025-10-08 13:57:20 +0200

    Read morse code mapping .json content to dictionary and test

--- MorseCodeTranslator/Program.cs
+++ MorseCodeTranslator/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace MorseCodeTranslator
+﻿using System.Text.Json;
+
+namespace MorseCodeTranslator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Get relative path to morse_code_mapping.json
+            var pathToMorseMapingJson = Path.Combine(AppContext.BaseDirectory, "Data", "morse_code_mapping.json");
+            // Read morse_come_mapping.json to a string
+            string morseMapingString = File.ReadAllText(pathToMorseMapingJson);
+            // Read morse_code_mapping and deserialize it to a dictionary
+            var morseEncodeDictionary = JsonSerializer.Deserialize<Dictionary<string, string>>(morseMapingString);
+            // Display all morse code mappings from the dictionary
+            foreach (var item in morseEncodeDictionary)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
         }
     }
 }
