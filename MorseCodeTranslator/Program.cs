5affcf82acf28dcda3c9000ae654659cdfe0d82b
Christiaan Huisman <huismanchristiaanwork@gmail.com>
2025-10-09 16:22:32 +0200

    Add sepeartion and modularity by createing services

--- MorseCodeTranslator/Program.cs
+++ MorseCodeTranslator/Program.cs
@@ -1,18 +1,18 @@
-﻿using System.Text.Json;
+﻿using MorseCodeTranslator.Services;
 
 namespace MorseCodeTranslator
 {
     internal class Program
     {
+        // Main MorseCodeTranslator program
         static void Main(string[] args)
         {
-            // Get relative path to morse_code_mapping.json
-            var pathToMorseMapingJson = Path.Combine(AppContext.BaseDirectory, "Data", "morse_code_mapping.json");
-            // Read morse_come_mapping.json to a string
-            string morseMapingString = File.ReadAllText(pathToMorseMapingJson);
-            // Read morse_code_mapping and deserialize it to a dictionary
-            var morseEncodeDictionary = JsonSerializer.Deserialize<Dictionary<string, string>>(morseMapingString);
-            // Display all morse code mappings from the dictionary
+            // Register the DataInitializationService
+            DataInitializationService dataInitializationService = new DataInitializationService();
+            // Get morseEncodeDictionary from the DataInitializationService LoadEncodeDictionary method
+            Dictionary<string, string> morseEncodeDictionary = dataInitializationService.LoadEncodeDictionary();
+
+            // Display all morse code mappings from the Dictionary for testing purposes
             foreach (var item in morseEncodeDictionary)
             {
                 Console.WriteLine($"{item.Key} : {item.Value}");
---
+++ MorseCodeTranslator/Services/DataInitializationService.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+namespace MorseCodeTranslator.Services
+{
+    // Initialization services for loading the encoding Dictionary and decoding binary tree
+    internal class DataInitializationService
+    {
+        // This string should not be changed after initialization
+        private readonly string _morseMappingString;
+        
+        // Constructor for string initialization
+        public DataInitializationService()
+        {
+            // Get relative path to morse_code_mapping.json for most OS types
+            var pathToMorseMappingJson = Path.Combine(AppContext.BaseDirectory, "Data", "morse_code_mapping.json");
+            // Read morse_come_mapping.json to a string
+            _morseMappingString = File.ReadAllText(pathToMorseMappingJson);
+        }
+
+        // Method for loading and returning the encoding Dicionary
+        public Dictionary<string, string> LoadEncodeDictionary()
+        {
+            // Read _morseMappingString and deserialize it to a dictionary
+            var morseEncodeDictionary = JsonSerializer.Deserialize<Dictionary<string, string>>(_morseMappingString);
+            // Return the dictionary
+            return morseEncodeDictionary;
+        }
+    }
+}
